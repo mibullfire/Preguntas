328f2b1efe0e7360d98e870d1654ea2e4ddab934
mibullfire <mibullfire@mail.com>
2024-10-10 13:02:58 +0200

    v0.2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Preguntas ISSI
+
+Para usar el programa, ejecute directamente desde la terminal el fichero 'main.py':
+
+```python
+python3 main.py
+```
+
+En la carpeta 'data' hay un fichero csv, puede cambiarlo con cualquier bloque de preguntas, siguiendo la estructura:
+
+```csv
+numero,tema,pregunta,respuestas,respuestas_correctas
+```
---
+++ main.py
@@ -0,0 +1,22 @@
+from src.preguntas import *
+
+if __name__ == "__main__":
+    # Leer el archivo CSV con las preguntas
+    import os
+    import tkinter as tk # Importar la librería tkinter
+
+    # Obtener la ruta absoluta del archivo CSV
+    script_dir = os.path.dirname(__file__)
+    csv_path = os.path.join(script_dir, 'data/solucionario.csv')
+
+    # Leer el archivo CSV con las preguntas
+    preguntas = leer_fichero(csv_path)
+
+    # Crear la ventana principal de la aplicación
+    root = tk.Tk()
+
+    # Crear la aplicación de preguntas
+    app = QuizApp(root, preguntas)
+
+    # Iniciar el bucle principal de la interfaz gráfica
+    root.mainloop()
